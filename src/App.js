b3d3261bdb96df33850191ace374fe5d34640ceb
Abirami Giri <abiramigiri1998@gmail.com>
2022-09-22 02:50:08 -0400

    a2 completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Abirami's Project
+            Abirami's Project A2
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
