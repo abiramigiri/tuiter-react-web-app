1b76847ea541935ad5abc6161bb8b9294c9d1a7f
Abirami Giri <abiramigiri@AbiramisMacBook.cable.rcn.com>
2022-10-23 15:24:50 -0400

    fixing link error a6 in navigation page

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -11,7 +11,7 @@ const NavigationSidebar = () => {
     return (
         <>
         <div className="list-group">
-            <a className="list-group-item">
+            <a href="#/" className="list-group-item">
                 <i className="bi bi-twitter"></i>
             </a>
             <Link to="/tuiter/home" className={`list-group-item ${active === 'home' ? 'active':''}`}>
@@ -22,27 +22,27 @@ const NavigationSidebar = () => {
                 <i className="fa-solid fa-hashtag pe-2"></i>
                 <span className="d-none d-xl-inline">Explore</span>
             </Link>
-            <a className={`list-group-item ${active === 'notifications' ? 'active':''}`}>
+            <a href="#/" className={`list-group-item ${active === 'notifications' ? 'active':''}`}>
                 <i className="fa-solid fa-bell pe-2"></i>
                 <span className="d-none d-xl-inline">Notifications</span>
             </a>
-            <a className={`list-group-item ${active === 'messages' ? 'active':''}`}>
+            <a href="#/" className={`list-group-item ${active === 'messages' ? 'active':''}`}>
                 <i className="fa-solid fa-envelope pe-2"></i>
                 <span className="d-none d-xl-inline">Messages</span>
             </a>
-            <a className={`list-group-item ${active === 'bookmarks' ? 'active':''}`}>
+            <a href="#/" className={`list-group-item ${active === 'bookmarks' ? 'active':''}`}>
                 <i className="fa-solid fa-bookmark pe-2"></i>
                 <span className="d-none d-xl-inline">Bookmarks</span>
             </a>
-            <a className={`list-group-item ${active === 'lists' ? 'active':''}`}>
+            <a href="#/" className={`list-group-item ${active === 'lists' ? 'active':''}`}>
                 <i className="fa-solid fa-list pe-2"></i>
                 <span className="d-none d-xl-inline">Lists</span>
             </a>
-            <a className={`list-group-item ${active === 'profile' ? 'active':''}`}>
+            <a href="#/" className={`list-group-item ${active === 'profile' ? 'active':''}`}>
                 <i className="fa-solid fa-user pe-2"></i>
                 <span className="d-none d-xl-inline">Profile</span>
             </a>
-            <a className={`list-group-item ${active === 'more' ? 'active':''}`}>
+            <a href="#/" className={`list-group-item ${active === 'more' ? 'active':''}`}>
                 <i className="fa-solid fa-circle-info pe-2"></i>
                 <span className="d-none d-xl-inline">More</span>
             </a>
